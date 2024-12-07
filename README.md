19376213a7ae5d1ef1ec09fd82b8aabab418abd9
Thang-Pham <hahanh29911301@gmail.com>
2024-12-07 09:11:43 +0700

    BT react_1

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite + Cybersoft
+
+B1: git clone https://github.com/NgoDuc2505/Cybersoft_ReactJs_b1_template
+B2: npm install
+
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import  {Header} from './components/header'
+import  {Banner} from './components/banner'
+import  {Item} from './components/item'
+import { Footer } from './components/footer'
+function App() {
+  
+  return (
+      <>
+        {/* Các bạn để các component ở trong đây !! */}
+        <Header></Header>
+        <Banner></Banner>
+        <Item></Item>
+        <Footer></Footer>
+      </>
+  )
+}
+
+export default App
+
